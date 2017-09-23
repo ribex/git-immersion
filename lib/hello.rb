3b57c1454a49aaf4565aedf230a20aebed3577ea
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2017-09-23 16:54:12 +0000

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,4 @@
-# Default is "World"
-# Author: Rebecca Wagaman (rebecca.wagaman@gmail.com)
-name = ARGV.first || "World"
+puts "What's your name"
+my_name = gets.strip
 
-puts "Hello, #{name}!"
+puts "Hello, #{my_name}!"
